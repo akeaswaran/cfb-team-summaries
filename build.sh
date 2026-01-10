accc531e70ebaded1a6d0b97c6d8502fb1e6886f
Akshay Easwaran <akeaswaran@me.com>
2026-01-10 17:03:57 -0500

    update build scripts

--- build.sh
+++ build.sh
@@ -8,9 +8,9 @@ while IFS= read -r line; do
   opts+=("id=${array[0]},env=${array[0]}")
 done < ./.env
 
-# echo "${opts}"
+echo "${opts}"
 DOCKER_BUILDKIT=1 docker build --secret "${opts[@]}" --platform=linux/amd64 -t cfb-team-summaries:latest .
 
 docker stop cfb-team-summaries
-docker remove cfb-team-summaries
+docker rm cfb-team-summaries
 docker run -it -p 3000:3000 --name=cfb-team-summaries --platform=linux/amd64 cfb-team-summaries:latest
